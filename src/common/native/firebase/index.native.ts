7be2ebb689831f666338327c4c32d7afea27cf92
Suraj Kiran S <spidi123q@gmail.com>
2022-09-02 19:18:52 +0530

    ios build fix

--- src/common/native/firebase/index.native.ts
+++ src/common/native/firebase/index.native.ts
@@ -1,10 +1,10 @@
 import Messaging from "@react-native-firebase/messaging";
 import Auth from "@react-native-firebase/auth";
-import Analytics from "@react-native-firebase/analytics";
+// import Analytics from "@react-native-firebase/analytics";
 import RemoteConfig from "@react-native-firebase/remote-config";
 
 export const messaging = Messaging;
 export const auth = Auth;
-export const analytics = Analytics;
+// export const analytics = Analytics;
 export const remoteConfig = RemoteConfig;
 export const isSupported = () => true;
